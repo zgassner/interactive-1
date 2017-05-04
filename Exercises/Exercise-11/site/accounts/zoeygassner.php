eb84e90f3dd669b99753168f51fae7027aea8b83
zgassner <Zoey@Zoeys-MacBook-Pro.local>
2017-05-04 15:46:27 -0700

    project 3

---
+++ Exercises/Exercise-11/site/accounts/zoeygassner.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: zoeygassner
+password: >
+  $2a$10$aApUhuTjMjaG4cnL00qVV.NZXWr/v8t2eN2OzBXteJPPrgwCfCXAO
+email: zoey.gassner@gmail.com
+language: en
+role: admin
